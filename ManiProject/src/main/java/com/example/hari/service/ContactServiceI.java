5e311a1ce9f3c4c2f4c285a63ebb487a0b027644
HariNagre <harishnagre42@gmail.com>
2022-08-24 18:00:31 +0530

    MIN-4:create service interface

--- ManiProject/src/main/java/com/example/hari/service/ContactServiceI.java
+++ ManiProject/src/main/java/com/example/hari/service/ContactServiceI.java
@@ -1,5 +1,18 @@
 package com.example.hari.service;
 
-public interface ContactServiceI {
+import java.util.List;
 
+import com.example.hari.entity.Contact;
+
+public interface ContactServiceI {
+ boolean saveContact(Contact contact);
+ 
+ List<Contact> getAllContact();
+ 
+ Contact getContactById(Integer cid);
+ 
+ public boolean updateContact(Contact contact);
+  
+ boolean deleteById(Integer cid);
+ 
 }
